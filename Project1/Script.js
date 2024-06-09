c922c6e5e070ca6a194d8a36b44230fbcf11d007
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-09 13:21:13 +0530

    Add Project

--- Project1/Script.js
+++ Project1/Script.js
@@ -3,4 +3,10 @@ const width=prompt("Please Enter Width")
 const Height=prompt("Please Enter Height")
 Area=(+Height* +width)
 console.log(Area)
-document.write(Area)
+
+
+const wi=+prompt("Please Enter Width")
+const He=+prompt("Please Enter Height")
+perimeter=2(wi+He)
+document.write(`Reactangle Permeter: ${perimeter}`)
+document.write(`Reactangle Area: ${Area}`)
