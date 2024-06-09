d845c3792657b9decdc8b8eec295b95c4ffd380b
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-09 14:09:10 +0530

    Add new file

---
+++ JavaScriptConcept/Datatype.js
@@ -0,0 +1,18 @@
+https://tc39.es/ecma262/
+
+//use Strict;-- to indicate that the code should be executed in "strict mode"
+console.log(typeof(alert()))
+
+let name="avneesh" // string
+
+let age=18 //number
+let isloggedIn =false// boolean
+//bigInt
+
+//null stand alone value(empty value) , Object
+// undefined = not assigend
+//symbol=>  for uniques  
+
+
+// Object
+
--- JavaScriptConcept/Varibles.js
+++ JavaScriptConcept/Varibles.js
@@ -1,8 +1,17 @@
-const accountId=123
-let accountEmail="kaviikumar010@gmail.com"
-var accountPass=2124
-accountCity="Bareilly"
-accountEmail="avneesh021@gmail.com"
-console.log(accountEmail)
+/*
+Prefer to not use Var
+Because  issue of block scope and Fuctional Scope
+*/
+
+const accountId = 123;
+
+let accountEmail = "kaviikumar010@gmail.com";
+var accountPass = 2124;
+accountCity = "Bareilly";
+accountEmail = "avneesh021@gmail.com";
+
+console.log(accountEmail);
 // accountId=23 // not allowed
 // console.log(accountId)
+
+console.table([accountId, accountEmail, accountPass, accountCity]);
