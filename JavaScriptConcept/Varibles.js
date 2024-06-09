78595b8274d32609fe4d048af396e457895b9d39
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-09 13:46:44 +0530

    new file

---
+++ JavaScriptConcept/Varibles.js
@@ -0,0 +1,8 @@
+const accountId=123
+let accountEmail="kaviikumar010@gmail.com"
+var accountPass=2124
+accountCity="Bareilly"
+accountEmail="avneesh021@gmail.com"
+console.log(accountEmail)
+// accountId=23 // not allowed
+// console.log(accountId)
