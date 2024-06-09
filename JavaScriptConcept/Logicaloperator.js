11809154b6e8b54b404b9219a82c88d273d35c69
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-09 23:28:32 +0530

    newChnage

---
+++ JavaScriptConcept/Logicaloperator.js
@@ -0,0 +1,26 @@
+
+/*
+&&(And) dono true
+|| or
+
+! not
+*/
+let a=(5<7)&&(7<8)
+console.log(a)
+
+// let user1=15
+// if((user1>=18) && (user1<=24))
+//     {
+//         console.log("True")
+//     }
+//     else
+
+//     console.log("false")
+
+const result=0&&2
+console.log(result)
+
+
+const result2=0||2
+console.log(result2)
+
