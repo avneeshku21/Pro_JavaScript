2cc2750c5c5f26e570365d93223da0f22cce3829
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-10 11:06:43 +0530

    new

---
+++ Project2/script.js
@@ -0,0 +1,30 @@
+const username=prompt('Please Enter Username')
+const userAge=+prompt('Please enter Age')
+
+if(userAge>=0 && userAge<=4)
+    {
+        console.log(`${username} is a kid`)
+        console.log('He Playing games')
+
+    }
+    if(userAge>=5 && userAge<=17)
+        {
+            console.log(`${username} is a School Student`)
+            console.log('He is learning  Science and Math')
+
+            
+        }
+        if(userAge>=18 && userAge<=23)
+            {
+                console.log(`${username} is a College Student`)
+                console.log('He is learning  computer Science ')
+                
+                
+            }
+            if(userAge>=24 && userAge<=60)
+                {
+                    console.log(`${username} is retired Pearson`)
+                    console.log('He is Enjoying rest of life ')
+                    
+                    
+                }
