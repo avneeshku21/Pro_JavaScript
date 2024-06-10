c221c19f3b5e7507f7e1d522f840282e8aab635c
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-10 10:24:46 +0530

    new

--- JavaScriptConcept/Decsion.js
+++
@@ -1,7 +0,0 @@
-
-<<<<<<< HEAD
-const user1=18
-const user2=20
-=======
-const user1=18
->>>>>>> dcd9cf16b8ff6ed636bb8742094fa2ed6c2fa3fa
---
+++ JavaScriptConcept/Descison.js
@@ -0,0 +1,6 @@
+const username='Avneesh'
+const userAge=21
+if(userAge>=18 && userAge<=45)
+    {
+        console.log(`${username} is a working professional`)
+    }
