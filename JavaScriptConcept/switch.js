1b0120797685d519658194609889ddfce1474e1d
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-10 11:47:04 +0530

    new

---
+++ JavaScriptConcept/switch.js
@@ -0,0 +1,29 @@
+const dayNum=3
+switch(dayNum)
+{
+    case 0:
+        console.log('Today Sunday')
+        break
+    case 1:
+        console.log('Today Monday')
+        break
+    case 2:
+        console.log('Today Tuesday')
+        break
+    case 3:
+         console.log('Today Wednesday')
+         break
+    case 4:
+         console.log('Today Thursday')
+         break
+     case 5:
+        console.log('Today Friday')
+        break
+    case 6:
+        console.log('Today Staturday')
+        break
+    default:
+        console.log("Program Ended")
+        
+}
+ 
