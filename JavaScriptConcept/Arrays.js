83f6d4e145448e78cc41798bf0ab1fe21b1f3d8f
Avneesh Kumar <kavneesh9045@gmail.com>
2024-06-11 12:15:35 +0530

    Update Arrays.js

--- JavaScriptConcept/Arrays.js
+++ JavaScriptConcept/Arrays.js
@@ -1,5 +1,6 @@
 // ++++++++++++++++++Array
 //JavaScript arrays are resizable and can contain a mix of different data types.
+//javaScript array-copy operations create shallow copies. 
 //JavaScript arrays are zero-indexed: the first element of an array is at index 0, the second is at index 1
 const fruits=['Apple', 'banana', 'grapes', 'orange', 'Mango']
 console.log(fruits)
