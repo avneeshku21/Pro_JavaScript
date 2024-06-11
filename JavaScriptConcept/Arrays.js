7c74c3e4816e389e0d011c1b9b60267c8a853c01
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-11 12:20:38 +0530

    new

--- JavaScriptConcept/Arrays.js
+++ JavaScriptConcept/Arrays.js
@@ -2,7 +2,19 @@
 //JavaScript arrays are resizable and can contain a mix of different data types.
 //javaScript array-copy operations create shallow copies. 
 //JavaScript arrays are zero-indexed: the first element of an array is at index 0, the second is at index 1
+/**
+ 1 Array type of is = Object
+
+
+ */
+
 const fruits=['Apple', 'banana', 'grapes', 'orange', 'Mango']
 console.log(fruits)
-console.table(fruits);
+//console.table(fruits);
 console.log(fruits.length)
+console.log(typeof fruits)
+
+console.log(fruits.concat('Peas'))
+
+
+
