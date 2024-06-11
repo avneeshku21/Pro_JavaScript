73cc3690e97272e05869f7764ae734068e4f4402
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-11 12:34:11 +0530

    new

--- JavaScriptConcept/Arrays.js
+++ JavaScriptConcept/Arrays.js
@@ -1,6 +1,6 @@
 // ++++++++++++++++++Array
 //JavaScript arrays are resizable and can contain a mix of different data types.
-//javaScript array-copy operations create shallow copies. 
+//javaScript array-copy operations create shallow copies.
 //JavaScript arrays are zero-indexed: the first element of an array is at index 0, the second is at index 1
 /**
  1 Array type of is = Object
@@ -8,13 +8,16 @@
 
  */
 
-const fruits=['Apple', 'banana', 'grapes', 'orange', 'Mango']
-console.log(fruits)
-//console.table(fruits);
-console.log(fruits.length)
-console.log(typeof fruits)
+const fruits = ["Apple", "banana", "grapes", "orange", "Mango"];
+// console.log(fruits);
+// //console.table(fruits);
+// console.log(fruits.length);
+// console.log(typeof fruits);
+// console.log(fruits.concat("Peas"));
+// fruits[3] = "watermelon";
+// console.log(fruits);
 
-console.log(fruits.concat('Peas'))
-
-
-
+const marvels=['thor','hulk','spiderman',{name:'Avneesh', age: 21, city:'bareilly'}]
+console.log(marvels)
+console.table(marvels);
+console.log(marvels[3].name)
