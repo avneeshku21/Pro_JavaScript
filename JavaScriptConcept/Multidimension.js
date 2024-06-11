f903f0dd12bfc928acf92456e2a7e938e42e3b42
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-11 14:33:20 +0530

    new

---
+++ JavaScriptConcept/Multidimension.js
@@ -0,0 +1,16 @@
+//So multidimensional arrays in JavaScript is known as arrays inside another array. 
+const data = [[1, 2, 3], [1, 3, 4], [4, 5, 6]];
+
+console.log(data);
+
+const newArr=[['Adrash'],['Avii','Kumar'],'Anisha']
+console.log(newArr)
+console.table(newArr);
+// const len=newArr[2].length
+// console.log(len)
+
+//Acces any element
+console.log(newArr[1][0])
+
+const ticTac=[['X',null,null],[null,null,'O'],['O',null,'X']]
+console.table(ticTac);
