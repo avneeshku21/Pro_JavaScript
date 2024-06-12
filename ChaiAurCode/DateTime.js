4a24f6f3d7b5bcfdeaee99b4cff5ed0d8db6b173
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-12 16:16:55 +0530

    new

---
+++ ChaiAurCode/DateTime.js
@@ -0,0 +1,23 @@
+//let myDate=new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+
+// console.log(typeof myDate)// Object
+
+// let myCreatedDate= new Date("2023-01-14")
+// console.log(myCreatedDate.toDateString())
+// console.log(myCreatedDate.toLocaleDateString())
+// console.log(myCreatedDate.toLocaleString())
+
+//+++++TimeStamp++++++++++
+// let mydate=new Date("01-14-2023")
+// let TimeStamp= Date.now()
+// console.log(TimeStamp);
+// console.log(mydate.getTime())
+//console.log(Math.floor(Date.now()/1000))
+
+let NewDate=new Date()
+console.log(NewDate)
+console.log(NewDate.getMonth()+1)
+console.log(NewDate.getDay())
