d3ba0722ca106eea8d310bbfbc1df2528cb5649b
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-12 12:45:23 +0530

    upadate

--- ChaiAurCode/MathNum.js
+++ ChaiAurCode/MathNum.js
@@ -1,4 +1,35 @@
+// //+++++++++++++Number++++++++++
+// let score=400
 
-const score=400
-const balance= new Number(100)
-console.log(balance)
+// let text =score.toString();
+// console.log(typeof text)
+// console.log(text)
+
+// const balance= new Number(100)
+// console.log(balance)
+// console.log(balance.toFixed(2))
+
+// const OtherNum=123.6546
+// console.log(OtherNum.toPrecision(3))
+
+// const hundres=100000
+// console.log(hundres.toLocaleString("en-IN"))
+
+//++++++++++++++++Maths++++++++++++++
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(6.3))
+// console.log(Math.ceil(4.67))
+// console.log(Math.sqrt(25))
+// console.log(Math.max(4,5,6,8,6))
+// console.log(Math.min(6,4,3,3,6,3,2))
+
+// Most Used.......... Math.random
+// important
+
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+const min=10
+const max=20+min
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
