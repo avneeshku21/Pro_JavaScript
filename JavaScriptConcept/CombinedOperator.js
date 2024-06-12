dff7a495a0b5c1e8e9f5518c5f4bd42bf33c06f0
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-12 10:34:32 +0530

    new

--- JavaScriptConcept/CombinedOperator.js
+++ JavaScriptConcept/CombinedOperator.js
@@ -1,1 +1,9 @@
-// Compound Operators
+/**
+ += is compound addition operator.
+-= is compound subtraction operator.
+*= is compound multiplication operator.
+/= is compound division operator.
+**= is compound exponential operator.
+%= is compound modulo operator.
+
+ */
