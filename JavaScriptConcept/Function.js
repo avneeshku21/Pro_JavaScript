6a329c0f9afa80ef3d410e723bb6f2d01279911f
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-13 00:34:57 +0530

    new

---
+++ JavaScriptConcept/Function.js
@@ -0,0 +1,31 @@
+//Function Defination
+// function introDuceme()
+// {
+//     console.log('hi')
+//     console.log("GOod Morning");
+//     console.log("Kaise ho");
+// }
+
+// Function call
+//const returnValue=introDuceme()
+// introDuceme()
+// introDuceme()
+ 
+// function Intro(username, job)
+
+// {
+//     console.log(`Hi i am ${username} , i am ${job} `)
+// }
+// Intro('Avneesh','Web developer')
+// Intro('Akash', 'Mechnical Engineer')
+
+//+++++++++++++++++++ Return Fuction++++++++++
+
+function addTwo(a,b)
+{
+    sum=a+b
+    return sum
+}
+const Sum=addTwo(addTwo(4,6),addTwo(8,12))
+
+console.log(Sum)
