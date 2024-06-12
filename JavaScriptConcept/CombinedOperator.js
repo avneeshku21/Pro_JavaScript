fb462486fe9a7795e8af048481412c25eee935f5
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-12 10:58:29 +0530

    new

--- JavaScriptConcept/CombinedOperator.js
+++ JavaScriptConcept/CombinedOperator.js
@@ -6,5 +6,15 @@
 /= is compound division operator.
 **= is compound exponential operator.
 %= is compound modulo operator.
-
  */
+let num=5
+
+num=++num+2
+num=++num+2
+num=++num+2
+num=++num+2
+
+console.log(num)
+let a=5
+a+=5
+console.log(a)
