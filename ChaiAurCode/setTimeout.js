f499982f512e34bb546a5c564bb8f4837d98fb36
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-15 11:40:17 +0530

    new

--- ChaiAurCode/setTimeout.js
+++ ChaiAurCode/setTimeout.js
@@ -1,3 +1,11 @@
 //SetTImeout and SetInterval
 //The global setTimeout() method sets a timer which executes a function or specified piece of code once 
-setTimeout('console.log("Avneesh")',2000)
+//setTimeout('console.log("Avneesh")',2000)
+//The setInterval() method calls a function at specified intervals (in milliseconds).
+//The setInterval() method continues calling the function until clearInterval() is called, or the window is closed.
+
+const timer1=setTimeout(a,3000)
+function a()
+{
+    console.log('Hello World')
+}
