a4b0bdba4635bf358a3787a6baeb918aeeebd292
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-15 11:23:21 +0530

    neww

---
+++ ChaiAurCode/setTimeout.js
@@ -0,0 +1,3 @@
+//SetTImeout and SetInterval
+//The global setTimeout() method sets a timer which executes a function or specified piece of code once 
+setTimeout('console.log("Avneesh")',2000)
--- JavaScriptConcept/setTimeout.js
+++
@@ -1,2 +0,0 @@
-//SetTImeout and SetInterval
-//The global setTimeout() method sets a timer which executes a function or specified piece of code once the timer expires.
