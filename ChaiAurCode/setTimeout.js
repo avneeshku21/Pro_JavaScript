38ee9c6603a4dda9aa199a2b437f5e16a9759eba
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-15 11:43:28 +0530

    new

--- ChaiAurCode/setTimeout.js
+++ ChaiAurCode/setTimeout.js
@@ -3,11 +3,13 @@
 //setTimeout('console.log("Avneesh")',2000)
 //The setInterval() method calls a function at specified intervals (in milliseconds).
 //The setInterval() method continues calling the function until clearInterval() is called, or the window is closed.
+
 //The clearInterval() Method
 
 //The setTimeout() Method
 
 //The clearTimeout() Method
+
 const timer1=setTimeout(a,3000)
 function a()
 {
