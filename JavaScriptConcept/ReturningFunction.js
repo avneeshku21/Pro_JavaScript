7f5fa9f096ddb0511c994dd2bdb489873d03a055
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-16 15:05:14 +0530

    neww

---
+++ JavaScriptConcept/ReturningFunction.js
@@ -0,0 +1,22 @@
+//Returning Function  Closures
+
+// function add()
+// {
+//     console.log(a+b)
+
+// }
+// add()
+// console.dir(add)
+function parent()
+{
+    const a=4
+const b=5
+    function add()
+{
+    console.log(a+b)
+
+}
+return add
+}
+const aadd1=parent()
+console.log(aadd1)
