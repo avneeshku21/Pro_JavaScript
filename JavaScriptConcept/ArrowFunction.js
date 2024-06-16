2809b82570a7c57242d4c20db27884aa90bda428
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-16 16:48:26 +0530

    neww

--- JavaScriptConcept/ArrowFunction.js
+++ JavaScriptConcept/ArrowFunction.js
@@ -8,8 +8,13 @@
 
 // Function Expression
 
-const squr=function square(num) {
-        return num*num
+// const squr=function square(num) {
+//         return num*num
         
- }
+//  }
+
+// Arrow Function Expression
+const squr=(num)=>{
+    
+}
  console.log(squr(4))
