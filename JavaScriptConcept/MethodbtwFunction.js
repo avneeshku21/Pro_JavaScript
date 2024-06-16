60f262ed25219c2cc7456fe4b9497900edee3be3
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-16 16:33:24 +0530

    newwupadte

---
+++ JavaScriptConcept/MethodbtwFunction.js
@@ -0,0 +1,15 @@
+
+const maths={
+    add:function(a,b){
+        return a+b
+    },
+    sub:function(b,c){
+        return b-c
+    },
+    mul:function(d,e){
+        return d*e
+    },
+}
+ console.log("Add=",maths.add(4,5))
+ console.log("sub=",maths.sub(6,5))
+ console.log("mul=  ",maths.mul(4,5))
