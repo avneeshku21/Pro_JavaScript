68471d6504a5f1fa2f00745b01c7a979b2ab5b9b
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-16 16:38:29 +0530

    new

---
+++ JavaScriptConcept/ArrowFunction.js
@@ -0,0 +1,15 @@
+
+//Function Decleration
+
+// function square(num) {
+//     return num*num
+    
+// }
+
+// Function Expression
+
+const squr=function square(num) {
+        return num*num
+        
+ }
+ console.log(squr(4))
