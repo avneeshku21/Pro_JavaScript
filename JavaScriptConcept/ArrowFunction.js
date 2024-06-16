385465a38b73525b8642426b053c7a92ffebb668
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-16 17:04:03 +0530

    new

--- JavaScriptConcept/ArrowFunction.js
+++ JavaScriptConcept/ArrowFunction.js
@@ -14,7 +14,15 @@
 //  }
 
 // Arrow Function Expression
+//This keyword current context k baare m batata hai
+//Execution Context
+// JavaScript Execution Context is the environment in which JavaScript code is executed. It contains information about the variables, functions, and objects that are available to the code being executed, as well as the scope chain and the value of the 'this' keyword.
+
+// An execution context has two phases:
+
+// creation phase
+// execution phase
 const squr=(num)=>{
-    
+
 }
  console.log(squr(4))
