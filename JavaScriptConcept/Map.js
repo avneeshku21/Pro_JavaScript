4eda2e741d45628f635024dc4a1678918f747412
Avneesh Kumar <kavneesh9045@gmail.com>
2024-06-17 11:32:44 +0530

    Update Map.js

--- JavaScriptConcept/Map.js
+++ JavaScriptConcept/Map.js
@@ -1,11 +1,11 @@
 //Description
-//map() creates a new array from calling a function for every array element.
+//map() creates a new array by calling a function for every array element.
 
 //map() does not execute the function for empty elements.
 
 //map() does not change the original array.
 
-const cars = ["Saab", "Volvo", "BMW","Nexon","I10"];
+const cars = ["Saab", "Volvo", "BMW", "Nexon", "I10"];
  const realArr=cars.map((cars)=>{
     console.log(cars)
     return cars.toUpperCase()
