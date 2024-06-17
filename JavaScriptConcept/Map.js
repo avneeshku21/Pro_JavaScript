0a89e71eac217dc4e884ecc819a1b9bb769c6d2f
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-17 11:56:29 +0530

    update

--- JavaScriptConcept/Map.js
+++ JavaScriptConcept/Map.js
@@ -5,9 +5,20 @@
 
 //map() does not change the original array.
 
-const cars = ["Saab", "Volvo", "BMW", "Nexon", "I10"];
- const realArr=cars.map((cars)=>{
-    console.log(cars)
-    return cars.toUpperCase()
- })
- console.log(realArr)
+// const cars = ["Saab", "Volvo", "BMW", "Nexon", "I10"];
+//  const realArr=cars.map((cars)=>{
+//     console.log(cars)
+//     return cars.toUpperCase()
+//  })
+//  console.log(realArr)
+
+// const courses = ["HTML", "CSS", "Javascript", "React","C++"];
+// const FilterCours=courses.filter((courses,index)=>{
+//     return courses.toUpperCase()
+// })
+
+const months=["January","Febuary","March","Aprial","May","June","July","August","September","Octeber","November","December"]
+const Filtermonths= months.filter((months,index)=>{
+    return (index,months.length>=5)
+})
+console.table(Filtermonths)
