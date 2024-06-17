693551b61cfbb8a8f69ef14c5bc7365ad8f92f95
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-17 17:37:08 +0530

    update

--- CounterProject/script.js
+++ CounterProject/script.js
@@ -1,19 +1,22 @@
 const  count=document.querySelector('.count')
 const  minusBtn=document.querySelector('.minus-btn')
 const  plusBtn=document.querySelector('.plus-btn')
+const changeBy=document.querySelector('.changeBy')
 
 
 minusBtn.addEventListener('click',()=>{
+    const changeByval=parseInt(changeBy.value)
     const countValue=parseInt(count.innerText)
     console.log(countValue)
-    count.innerText=countValue-1
+    count.innerText=countValue-changeByval
     })
     
     
 
 plusBtn.addEventListener('click',()=>{
+const changeByval=parseInt(changeBy.value)
 const countValue=parseInt(count.innerText)
 console.log(countValue)
-count.innerText=countValue+1
+count.innerText=countValue+changeByval
 })
 
