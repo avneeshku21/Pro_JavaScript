3db3731e6e240477ec9d14afdf1ce85fe35bd324
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-19 23:13:13 +0530

    Update

---
+++ PatternQuestion/Fifth.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(char i='A'; i<='E'; i++)
+    {
+        for(char j='A'; j<='E'; j++)
+        {
+cout<<i;
+
+        }
+        
+        cout<<endl;
+    }
+
+}
