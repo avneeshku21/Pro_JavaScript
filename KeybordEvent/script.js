70977162c9d29a39877be03dcbefa00a7d8f7727
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-21 11:57:12 +0530

    upadate

---
+++ KeybordEvent/script.js
@@ -0,0 +1,22 @@
+const h1=document.querySelector('h1')
+const input=document.querySelector('input')
+
+
+
+input.addEventListener('keypress',(e)=>{
+    console.log(e.key)
+
+})
+
+h1.addEventListener('keypress',(e)=>{
+    console.log(e)
+
+})
+
+
+
+/*******
+Jo focus element nhi hote uske liye hame = tabindex dena hota hai 
+
+
+*/
