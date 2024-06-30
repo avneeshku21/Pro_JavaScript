3d3038275cfb44a3228c75154974c0b1a50891d1
Avneesh Kumar <kavneesh9045@gmail.com>
2024-06-30 13:21:36 +0530

    Update ArrowFunction.js

--- JavaScriptConcept/ArrowFunction.js
+++ JavaScriptConcept/ArrowFunction.js
@@ -14,7 +14,7 @@
 //  }
 
 // Arrow Function Expression
-//This keyword current context k baare m batata hai
+// This keyword current context ke baare m batata hai
 //Execution Context
 // JavaScript Execution Context is the environment in which JavaScript code is executed. It contains information about the variables, functions, and objects that are available to the code being executed, as well as the scope chain and the value of the 'this' keyword.
 
