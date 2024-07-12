dfd8d15e8a4ae508cbd4b2aff3501ef17b4af819
Avneesh Kumar <kaviikumar010@gmail.com>
2024-07-12 15:28:37 +0530

    update

---
+++ ChaiAurCode/Promises.js
@@ -0,0 +1,66 @@
+// const promiseOne=new Promise(function(resolve,reject){
+//     //Do an Async task
+//     //DB Calls , Cryptography , network calls
+
+//     setTimeout(function(){
+//         console.log('Async Task is Complete')
+//         resolve()
+//     },1000)
+// })
+// promiseOne.then(function()
+// {
+//     console.log('Promise Consume')
+// })
+
+
+// new Promise(function(resolve,reject){
+//     setTimeout(function(){
+// console.log('Async Task 2')
+// resolve()
+//     },1000)
+// })
+// .then(function()
+// {
+//     console.log('resolve')
+// })
+
+
+// const promisethree=new Promise(function(resolve,reject){
+//     setTimeout(function(){
+
+//         resolve({
+//             username: "avneesh",
+//             email:"kaviikumar010@gmail.com"
+//         })
+
+//     },2000)
+// })
+
+// promisethree.then(function(user){
+
+// console.table(user)
+// })
+
+const promiseFour=new Promise(function(resolve,reject){
+let error=true
+if(!error==false){
+    resolve({username:"Avii", Password:"123"})
+}
+
+else{
+    reject('Error')
+}
+
+},1000)
+
+promiseFour
+.then(function(user){
+console.log(user)
+return user.username
+})
+.then((username)=>{
+console.log(username)
+})
+.catch(function(err){
+    console.log(err)
+})
