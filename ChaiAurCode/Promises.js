299626cd36877de96f8a86c23c2e15ace1a129b2
Avneesh Kumar <kaviikumar010@gmail.com>
2024-07-12 15:43:29 +0530

    update

--- ChaiAurCode/Promises.js
+++ ChaiAurCode/Promises.js
@@ -41,26 +41,50 @@
 // console.table(user)
 // })
 
-const promiseFour=new Promise(function(resolve,reject){
-let error=true
-if(!error==false){
-    resolve({username:"Avii", Password:"123"})
+// const promiseFour=new Promise(function(resolve,reject){
+// let error=true
+// if(!error==false){
+//     resolve({username:"Avii", Password:"123"})
+// }
+
+// else{
+//     reject('Error')
+// }
+
+// },1000)
+
+// promiseFour
+// .then(function(user){
+// console.log(user)
+// return user.username
+// })
+// .then((username)=>{
+// console.log(username)
+// })
+// .catch(function(err){
+//     console.log(err)
+// }).finally(()=>{
+//     console.log('Finally Resolved')
+// })
+
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if (!error) {
+            resolve({username: "javascript", password: "123"})
+        } else {
+            reject('ERROR: JS went wrong')
+        }
+    }, 1000)
+});
+
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-else{
-    reject('Error')
-}
-
-},1000)
-
-promiseFour
-.then(function(user){
-console.log(user)
-return user.username
-})
-.then((username)=>{
-console.log(username)
-})
-.catch(function(err){
-    console.log(err)
-})
+consumePromiseFive()
